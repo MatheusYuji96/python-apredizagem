9c0b4c0521424077b6d818e560b3792c30a448da
Matheus Yuji <matheus.yamakuti@sptech.school>
2026-03-11 14:12:32 +0000

    Adicionando 3 e 4

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,15 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print('Bom dia ' * 3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+jogos = 15
+mediaHoraJogo = 9.3
+tempoTotal = jogos * mediaHoraJogo
+print(tempoTotal)
--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,25 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+idade = 2010 - 1989
+print(idade)
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
+a = 3
+b = 10
+c = 10
+d = 7
 
+print(a > d)
+print(b == c)
+print(d <= c)
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+a = 3
+b = 10
+c = 10
+d = 7
+
+print(a > d or b > d)
+print(d >= b and c >= b)
+print(a != b and c != d)
