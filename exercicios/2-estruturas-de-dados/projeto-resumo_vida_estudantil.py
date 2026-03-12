3831438489357301bae5f1b8ff1eb5f9bb8f2a2f
Matheus Yuji <matheus.yamakuti@sptech.school>
2026-03-12 17:54:12 +0000

    Adicionando vida estudantil

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -3,3 +3,12 @@
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
 # 2. Armazene esses dados em um dicionário
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
+
+estudante = {}
+estudante['nome'] = input('Qual o seu nome? ')
+estudante['ano_LinkedIn'] = input('Qual o ano que conheceu o LinkedIn Learning? ')
+estudante['ano_atual'] = input('Qual o ano atua? ')
+cursos = input('Quais os cursos que fez na plataforma? (Separe-os com vírgula[,] e espaço) ')
+
+estudante['cursos'] = cursos.split(', ')
+print(f"O/A estudante {estudante['nome']} conheceu o LinkedIn Learning no ano de {estudante['ano_LinkedIn']}. Agora é {estudante['ano_atual']} e já fez os cursos de {estudante['cursos']}")
