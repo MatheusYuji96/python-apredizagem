e0eb91072d6b702fbdfe41e5f4caac0abd390d27
Matheus Yuji <matheus.yamakuti@sptech.school>
2026-03-13 14:28:36 +0000

    Adicionando while

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,15 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
+faseAtual = 5
+faseFinal = 15
 
 # Crie um while loop que imprima na tela o nível atual
-
+while faseAtual <= faseFinal:
+  print(faseAtual)
+  faseAtual += 1
 
 # Insira "else" no while loop anterior.
+while faseAtual <= faseFinal:
+  print(faseAtual)
+  faseAtual += 1
+else:
+  print("Você concluiu o curso")
